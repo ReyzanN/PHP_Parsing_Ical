5b716f0e91ea89fd9f4157f8c7513a8a5f6be3e3
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-01-09 09:48:14 +0100

    Add old link file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'icalGenerator.php';
-$link = '';
+$link = 'maison_au_loup_chambre_bromelia.ics';
 $from = 'Booking.com';
 $ical = new iCalGenerator(array('fileLink' => $link, 'icalFrom' => $from));
 echo '<pre>';
